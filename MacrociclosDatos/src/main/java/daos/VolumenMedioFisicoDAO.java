4248207003939ed4847747036c2ff27257c15051
Ganz0k <luisgonzalocervantes@gmail.com>
2023-10-30 20:13:24 -0700

    Creacion de clase VolumenMedioFisicoDAO

---
+++ MacrociclosDatos/src/main/java/daos/VolumenMedioFisicoDAO.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package daos;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import conexion.ConexionBD;
+import entidades.Macrociclo;
+import entidades.Mesociclo;
+import entidades.VolumenMedioFisico;
+import java.util.List;
+import org.bson.types.ObjectId;
+
+/**
+ *
+ * @author luisg
+ */
+public class VolumenMedioFisicoDAO {
+    
+    private final MongoDatabase baseDatos;
+    
+    public VolumenMedioFisicoDAO() {
+        this.baseDatos = ConexionBD.crearConexion();
+    }
+    
+    private MongoCollection<Macrociclo> getColeccionFormatoMacrociclo() {
+        return this.baseDatos.getCollection("macrociclo", Macrociclo.class);
+    }
+    
+    public boolean guardarVolumenMedioFisicoEnMesociclo(ObjectId idMacrociclo, ObjectId idMesociclo, VolumenMedioFisico volumenMedioFisico) {
+        try {
+            MongoCollection<Macrociclo> coleccion = this.getColeccionFormatoMacrociclo();
+            Macrociclo macrociclo = coleccion.find(Filters.eq("_id", idMacrociclo)).first();
+            
+            if (macrociclo != null) {
+                List<Mesociclo> mesociclos = macrociclo.getMesociclos();
+
+                for (int i = 0; i < mesociclos.size(); i++) {            
+                    if (mesociclos.get(i).getId().equals(idMesociclo)) {
+                        mesociclos.get(i).getDistribucionVolumen().add(volumenMedioFisico);
+                        macrociclo.setMesociclos(mesociclos);
+                        break;
+                    }
+                }
+
+                coleccion.findOneAndReplace(Filters.eq("_id", idMacrociclo), macrociclo);
+                return true;
+            }
+            
+            return false;
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            System.err.println(e.getCause());
+            return false;
+        }
+    }
+}
