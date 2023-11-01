91c1c0c7e788fa87be21b5b927bbfa6f58fff5b8
Ganz0k <luisgonzalocervantes@gmail.com>
2023-10-31 20:03:50 -0700

    small fix

--- MacrociclosDatos/src/main/java/daos/VolumenMedioFisicoDAO.java
+++ MacrociclosDatos/src/main/java/daos/VolumenMedioFisicoDAO.java
@@ -19,37 +19,37 @@ import org.bson.types.ObjectId;
  * @author luisg
  */
 public class VolumenMedioFisicoDAO {
-    
+
     private final MongoDatabase baseDatos;
-    
+
     public VolumenMedioFisicoDAO() {
         this.baseDatos = ConexionBD.crearConexion();
     }
-    
+
     private MongoCollection<Macrociclo> getColeccion() {
         return this.baseDatos.getCollection("macrociclos", Macrociclo.class);
     }
-    
-    public boolean guardarVolumenMedioFisicoEnMesociclo(ObjectId idMacrociclo, ObjectId idMesociclo, VolumenMedioFisico volumenMedioFisico) {
+
+    public boolean guardarVolumenMedioFisicoEnMesociclo(ObjectId idMacrociclo, ObjectId idMesociclo, List<VolumenMedioFisico> volumenesMediosFisicos) {
         try {
             MongoCollection<Macrociclo> coleccion = this.getColeccion();
             Macrociclo macrociclo = coleccion.find(Filters.eq("_id", idMacrociclo)).first();
-            
+
             if (macrociclo != null) {
                 List<Mesociclo> mesociclos = macrociclo.getMesociclos();
 
-                for (int i = 0; i < mesociclos.size(); i++) {            
+                for (int i = 0; i < mesociclos.size(); i++) {
                     if (mesociclos.get(i).getId().equals(idMesociclo)) {
-                        mesociclos.get(i).getDistribucionVolumen().add(volumenMedioFisico);
+                        mesociclos.get(i).setDistribucionVolumen(volumenesMediosFisicos);
                         macrociclo.setMesociclos(mesociclos);
                         break;
-                    }
+                    } 
                 }
 
                 coleccion.findOneAndReplace(Filters.eq("_id", idMacrociclo), macrociclo);
                 return true;
             }
-            
+
             return false;
         } catch (Exception e) {
             System.err.println(e.getMessage());
